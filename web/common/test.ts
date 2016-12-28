34bb0b4946d8ae85989b475159530b8663db26bf
gguo <gguo@movoto.com>
2016-12-28 17:12:28 +0800

    updated

--- web/common/test.ts
+++ web/common/test.ts
@@ -12,14 +12,24 @@ class Computer implements iAdd {
     }
 }
 
+class Computer2 implements iAdd {
+    add(a: number): number {
+        return a + 1;
+    }
+
+    addX(a: number): number {
+        return a + 100;
+    }
+}
+
 var computer = new Computer();
 console.log(computer.add(1));
 console.log(computer.add10(1));
 
 // for now everything is working, however what i want is below,
-
-let methodName:string = 'add10';
-// computer[methodName]();
+type FNs = keyof Computer;
+let methodName:FNs = 'add10';
+computer[methodName](1);
 
 // now there is an error: [ts] Element implicity has an 'any' type because type 'Computer' has no index signature
 // I don't want to use this way to fix, i want a safer way.
