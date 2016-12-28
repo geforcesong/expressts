3d78d075956ead53249231fddd477224fbb69818
gguo <gguo@movoto.com>
2016-12-28 16:02:40 +0800

    added test

--- web/common/test.ts
+++ web/common/test.ts
@@ -16,3 +16,12 @@ var computer = new Computer();
 console.log(computer.add(1));
 console.log(computer.add10(1));
 
+// for now everything is working, however what i want is below,
+
+let methodName:string = 'add10';
+computer[methodName]();
+
+// now there is an error: [ts] Element implicity has an 'any' type because type 'Computer' has no index signature
+// I don't want to use this way to fix, i want a safer way.
+
+(<any>computer)[methodName](); // don't want this fix
