6bb41f04a6869e091b49491d673d0563186fbad9
gguo <gguo@movoto.com>
2016-12-28 13:17:40 +0800

    different ts config

--- web/common/validator.ts
+++ web/common/validator.ts
@@ -1,6 +1,6 @@
 class Validator {
     static isEmptyString(input: string) {
-        if (input && input.length > 0)
+        if (input && input.length > 110)
             return false;
         return true;
     }
