ef0e9d54b1d968aebb0a597b35f758d881a39594
gguo <gguo@movoto.com>
2017-01-05 15:51:02 +0800

    added less support

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
+const gulp = require('gulp');
+const less = require('gulp-less');
+var concat = require('gulp-concat');
+const packageObject = require('./package.json');
+
 gulp.task("compile less",function(){
+    var fileName = `site.${packageObject.version}.css`
     gulp.src(['./web/views/**/*.less'])
         .pipe(less())
-        .pipe(gulp.dest('./web/views/'));
+        .pipe(concat(fileName))
+        .pipe(gulp.dest('./public/build'));
 })
 
 gulp.task('default', ['compile less']);
