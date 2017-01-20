b07ac152bf75f281607496b0435fc5221f98f18d
gguo <gguo@movoto.com>
2017-01-20 14:15:59 +0800

    updated

---
+++ web/views/home/client/app/todo-item.vue
@@ -0,0 +1,14 @@
+<template>
+    <li class="txt">{{todo.text}}</li>
+</template>
+
+<script>
+    export default {
+        props: ['todo']
+    }
+</script>
+<style scoped>
+    .txt {
+        color: blue;
+    }
+</style>
