9b689a2ff0fd71dfca43fc858458a3b7d206cff2
gguo <gguo@movoto.com>
2017-02-21 10:17:24 +0800

    added 3rd code

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var resolve = require('path').resolve;
 var webpack = require('webpack');
-
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     entry: {
         index: [
@@ -23,6 +23,10 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel?{"presets":["es2015"]}',
                 exclude: /node_modules/
+            },
+            { 
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
         ]
     },
@@ -45,5 +49,12 @@ module.exports = {
             '.js',
             '.vue'
         ]
-    }
+    },
+    plugins:[
+        new CopyWebpackPlugin([
+            { from: 'node_modules/bootstrap/dist/js/bootstrap.min.js', to: 'libs/bootstrap/bootstrap.min.js' },
+            { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: 'libs/bootstrap/bootstrap.min.css' },
+            { from: 'node_modules/jquery/dist/jquery.min.js', to: 'libs/jquery/jquery.min.js' }
+        ])
+    ]
 };
